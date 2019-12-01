af63cd22ca25b4ce4aaa93da7030ec9ee3d81fbf
Paul Greveson <p.greveson@gmail.com>
2019-12-01 14:58:34 +0100

    Use unittest module for testing, why not

--- 2019/01/01.py
+++ 2019/01/01.py
@@ -2,35 +2,38 @@
 
 from math import floor
 from os import path
+import unittest
+
+
+class TestFuelCalculation(unittest.TestCase):
+    def test_fuel(self):
+        _tests = [
+            (12, 2),
+            (14, 2),
+            (1969, 654),
+            (100756, 33583),
+        ]
+        for test in _tests:
+            self.assertEqual(_get_fuel_for_mass(test[0]), test[1])
 
 
 def _get_fuel_for_mass(mass):
     return floor(mass / 3) - 2
 
 
-_tests = [
-    (12, 2),
-    (14, 2),
-    (1969, 654),
-    (100756, 33583),
-]
-_num_tests_passed = 0
-for test in _tests:
-    fuel = _get_fuel_for_mass(test[0])
-    if fuel == test[1]:
-        _num_tests_passed += 1
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFuelCalculation)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if len(result.failures):
+        print(f"{len(result.failures)} tests failed!")
     else:
-        print(f"Test failed for mass {test[0]}: Expected {test[1]}, got {fuel}")
-
-if _num_tests_passed == len(_tests):
-    print(f"All {_num_tests_passed} tests passed!")
-
-    input_file = path.join(path.dirname(__file__), "input.txt")
-    total_fuel = 0
-    with open(input_file, 'r') as input:
-        mass = input.readline()
-        while mass:
-            total_fuel += _get_fuel_for_mass(int(mass))
+        print(f"All tests passed!")
+        input_file = path.join(path.dirname(__file__), "input.txt")
+        total_fuel = 0
+        with open(input_file, 'r') as input:
             mass = input.readline()
+            while mass:
+                total_fuel += _get_fuel_for_mass(int(mass))
+                mass = input.readline()
 
-    print(f"Total mass: {total_fuel}")
+        print(f"Total mass: {total_fuel}")
