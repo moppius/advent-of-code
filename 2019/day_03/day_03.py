d98a04b8ec0fbb0aab9921c1223e9757722a052c
Paul Greveson <p.greveson@gmail.com>
2019-12-04 23:11:26 +0100

    Day 3: Solution to part one

--- 2019/day_03/day_03.py
+++ 2019/day_03/day_03.py
@@ -32,15 +32,68 @@ class ChallengeTests(TestCase):
             self.assertEqual(WireIntersector(test[0]).get_nearest_result(), test[1])
 
 
+class WireSection():
+    """Class that represents a section of wire."""
+
+    def __init__(self, start, direction, length):
+        self.start = start.copy()
+        if direction == "U":
+            start[1] += length
+        elif direction == "D":
+            start[1] -= length
+        elif direction == "L":
+            start[0] -= length
+        elif direction == "R":
+            start[0] += length
+        self.end = start.copy()
+
+        # Reverse order
+        if direction in ["D", "L"]:
+            temp = self.start
+            self.start = self.end
+            self.end = temp
+
+    def __repr__(self):
+        return f"Wire section starting at {self.start} and ending at {self.end}"
+
+
 class WireIntersector():
     """Class for finding closest Manhattan distance of crossing wires from a common center."""
 
     def __init__(self, wires):
-        pass
+        self.wire1 = self._process_wire(wires[0])
+        self.wire2 = self._process_wire(wires[1])
+
+    @staticmethod
+    def _process_wire(wire):
+        result = {"horizontal": [], "vertical": []}
+        start = [0, 0]
+        for section in wire:
+            direction = section[0]
+            if direction not in ["U", "D", "L", "R"]:
+                print(f"Error: Invalid direction '{direction}'")
+                return None
+            length = int(section[1:])
+            angle = "vertical" if direction in ["U", "D"] else "horizontal"
+            result[angle].append(WireSection(start, direction, length))
+        return result
 
     def get_nearest_result(self):
         """Return the nearest crossing result's Manhattan distance from center."""
-        return 0
+        crossings = []
+        for wires in [(self.wire1, self.wire2), (self.wire2, self.wire1)]:
+            for horizontal in wires[0]["horizontal"]:
+                for vertical in wires[1]["vertical"]:
+                    test1 = vertical.start[0] > horizontal.start[0] and vertical.end[0] < horizontal.end[0]
+                    test2 = vertical.start[1] < horizontal.start[1] and vertical.end[1] > horizontal.start[1]
+                    if test1 and test2:
+                        crossings.append([abs(vertical.start[0]), abs(horizontal.start[1])])
+        shortest = None
+        for crossing in crossings:
+            distance = crossing[0] + crossing[1]
+            if shortest is None or distance < shortest:
+                shortest = distance
+        return shortest
 
 
 def run_challenge():
