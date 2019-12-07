3f7609c5ea5d73bb6b14f3c76d702b1462f1370e
Paul Greveson <p.greveson@gmail.com>
2019-12-08 00:46:31 +0100

    Day 6: Solution to part two
    Also added test 2 input as a text file

--- 2019/day_06/day_06.py
+++ 2019/day_06/day_06.py
@@ -17,6 +17,16 @@ class ChallengeTests(TestCase):
         for test in tests:
             self.assertEqual(_get_orbit_count(_read_orbits(test[0])), test[1])
 
+    def test_part2(self):
+        """Test part two example values."""
+
+        tests = [
+            ("test_2_input.txt", 4),
+        ]
+
+        for test in tests:
+            self.assertEqual(_get_min_transfer_count(_read_orbits(test[0])), test[1])
+
 
 class SpaceObject():
     """An object in space which may orbit something, and has child objects which orbit it."""
@@ -28,15 +38,15 @@ class SpaceObject():
     def __eq__(self, other):
         return self.name == other.name
 
-    def get_orbits(self, space_objects, start_count):
-        """Returns the number of direct & indirect orbits from this space object."""
+    def get_parent_ids(self, space_objects, parent_ids):
+        """Fills the `parent_ids` list with all parents."""
         if self.parent_id is None:
-            return start_count
-        start_count += 1
-        return space_objects[self.parent_id].get_orbits(space_objects, start_count)
+            return
+        parent_ids.append(self.parent_id)
+        space_objects[self.parent_id].get_parent_ids(space_objects, parent_ids)
 
 
-def _get_orbit_count(orbit_list):
+def _build_space_objects(orbit_list):
     space_objects = []
     for orbit in orbit_list:
         obj1 = SpaceObject(orbit[0])
@@ -49,11 +59,38 @@ def _get_orbit_count(orbit_list):
             space_objects.append(obj2)
         else:
             space_objects[space_objects.index(obj2)].parent_id = parent_id
+    return space_objects
 
+
+def _get_orbit_count(orbit_list):
+    space_objects = _build_space_objects(orbit_list)
     total_orbits = 0
     for space_object in space_objects:
-        total_orbits += space_object.get_orbits(space_objects, 0)
+        parent_ids = []
+        space_object.get_parent_ids(space_objects, parent_ids)
+        total_orbits += len(parent_ids)
     return total_orbits
+
+
+def _get_min_transfer_count(orbit_list):
+    space_objects = _build_space_objects(orbit_list)
+    start_parent_ids = []
+    end_parent_ids = []
+    for space_object in space_objects:
+        if space_object.name == "YOU":
+            space_object.get_parent_ids(space_objects, start_parent_ids)
+        if space_object.name == "SAN":
+            space_object.get_parent_ids(space_objects, end_parent_ids)
+
+    smallest_distance = None
+    for i, start_id in enumerate(start_parent_ids):
+        for j, end_id in enumerate(end_parent_ids):
+            if start_id == end_id:
+                this_distance = i + j
+                if smallest_distance is None or this_distance < smallest_distance:
+                    smallest_distance = this_distance
+
+    return smallest_distance
 
 
 def _read_orbits(filename):
@@ -67,5 +104,8 @@ def _read_orbits(filename):
 def run_challenge():
     """Run the challenge."""
 
-    part_one_solution = _get_orbit_count(_read_orbits("input.txt"))
+    orbits = _read_orbits("input.txt")
+    part_one_solution = _get_orbit_count(orbits)
     print(f"\tPart one solution: {part_one_solution}")
+    part_two_solution = _get_min_transfer_count(orbits)
+    print(f"\tPart two solution: {part_two_solution}")
