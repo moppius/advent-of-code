d3e9df7d3615e1c4b10e967b22012f0e842befff
Paul Greveson <p.greveson@gmail.com>
2020-12-03 00:03:50 +0100

    Moved the code that runs challenges to the root
    Also updated documentation to match

--- 2019/__main__.py
+++
@@ -1,8 +0,0 @@
-"""Main entry point for Advent of Code 2019 module."""
-
-
-import sys
-from . import main
-
-
-main(sys.argv[1:])
--- advent-of-code.py
+++ advent-of-code.py
@@ -1,4 +1,4 @@
-"""Advent of Code 2019 module."""
+"""Advent of Code 2020 module."""
 
 
 from argparse import ArgumentParser
@@ -14,38 +14,36 @@ RUN_METHOD_NAME = "run_challenge"
 def main(args=None):
     """Parse arguments and run challenges."""
 
-    parser = ArgumentParser(description="Run Advent of Code 2019 challenges.")
+    parser = ArgumentParser(description="Run Advent of Code 2020 challenges.")
+    parser.add_argument("-y", "--year", help="choose the year")
     parser.add_argument("-d", "--day", help="run a specific day's challenge")
     args = parser.parse_args(args)
-    run_challenges(args.day)
+    run_challenges(args.year, args.day)
 
 
-def run_challenges(only_day=None):
+def run_challenges(year, only_day=None):
     """Run all challenges if `only_latest` is False, otherwise run the latest day's challenge."""
 
-    days = [obj for obj in listdir(path.dirname(__file__)) if obj.startswith("day_")]
+    days = [obj for obj in listdir(path.join(path.dirname(__file__), year)) if obj.startswith("day_")]
     found_day = False
     for day in days:
         if only_day is not None:
             if int(day.split("_")[1]) == int(only_day):
                 found_day = True
-                _run_challenge(day)
+                _run_challenge(year, day)
                 break
         else:
-            _run_challenge(day)
+            _run_challenge(year, day)
     if only_day is not None and not found_day:
         print(f"Error: Failed to find module for day {only_day}!")
 
 
-def _run_challenge(day):
+def _run_challenge(year, day):
     """Run the challenge for a given day."""
 
     name = f"Day {day.split('_')[1]}"
     print(f"\n\n{name}")
-    if __name__ == "__main__":
-        module = import_module(f"{day}.{day}", ".")
-    else:
-        module = import_module(f".{day}.{day}", "2019")
+    module = import_module(f".{day}.{day}", year)
     method = getattr(module, RUN_METHOD_NAME, None)
     if method is None:
         print(f"Error: {name} has no '{RUN_METHOD_NAME}' method!")
